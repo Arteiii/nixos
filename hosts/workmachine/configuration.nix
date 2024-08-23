a86607f043da1b2e6d8d91aab16a75155acbad37
Arteii <ben.arteii@proto.me>
2024-08-23 11:18:48 +0200

    Initial commit

---
+++ hosts/workmachine/configuration.nix
@@ -0,0 +1,153 @@
+# Edit this conguration file to define what should be installed on
+# your system.  Help is available in the conguration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];  
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Dene your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Congure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Congure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+  # Congure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redene it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Dene a user account. Don't forget to set a password with ‘passwd’.
+  users.users.arteii = {
+    isNormalUser = true;
+    description = "Ben";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  managed by home manager /etc/nixos/users/arteii/home.nix
+    ];
+  };
+
+  home-manager = {
+    backupFileExtension = "backup";
+    users = {
+      "arteii" = import ../../users/arteii/home.nix;
+    };
+  };
+
+  # set prole pciture for arteii
+  system.activationScripts.script.text = ''
+    mkdir -p /var/lib/AccountsService/{icons,users}
+    cp /etc/nixos/users/arteii/prolepicture.png /var/lib/AccountsService/icons/arteii
+    echo -e "[User]\nIcon=/var/lib/AccountsService/icons/arteii\n" > /var/lib/AccountsService/users/MY_USER_NAME
+
+    chown root:root /var/lib/AccountsService/users/arteii
+    chmod 0600 /var/lib/AccountsService/users/arteii
+
+    chown root:root /var/lib/AccountsService/icons/arteii
+    chmod 0444 /var/lib/AccountsService/icons/arteii
+  '';
+
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system prole. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
+  # Some programs need SUID wrappers, can be congured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the rewall.
+  # networking.rewall.allowedTCPPorts = [ ... ];
+  # networking.rewall.allowedUDPPorts = [ ... ];
+  # Or disable the rewall altogether.
+  # networking.rewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like le locations and database versions
+  # on your system were taken. It‘s perfectly ne and recommended to leave
+  # this value at the release version of the rst install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man conguration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ users/arteii/firefox.nix
@@ -0,0 +1,99 @@
+
+
+# https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265/7
+{ config, pkgs, ... }:
+
+  let
+    lock-false = {
+      Value = false;
+      Status = "locked";
+    };
+    lock-true = {
+      Value = true;
+      Status = "locked";
+    };
+  in
+{
+  programs = {
+    firefox = {
+      enable = true;
+      package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+        extraPolicies = {
+          DisableTelemetry = true;
+          # add policies here...
+
+          /* ---- EXTENSIONS ---- */
+          ExtensionSettings = {
+            # uBlock Origin:
+            "uBlock0@raymondhill.net" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+              installation_mode = "force_installed";
+            };
+	    
+	    "languagetool-webextension@languagetool.org" = {
+	      install_url = "https://addons.mozilla.org/firefox/downloads/latest/languagetool/latest.xpi";
+	      installation_mode = "force_installed";
+            };
+	    # Privacy Badger:
+            "jid1-MnnxcxisBPnSXQ@jetpack" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+              installation_mode = "force_installed";
+            };
+	    # Disconnect
+            "2.0@disconnect.me" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/disconnect/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # Ghostery
+	    "firefox@ghostery.com" = {
+	      install_url = "https://addons.mozilla.org/firefox/downloads/latest/ghostery/latest.xpi";
+	      installation_mode = "force_install";
+	    };
+            # add extensions here...
+          };
+  
+          /* ---- PREFERENCES ---- */
+          # Set preferences shared by all profiles.
+          Preferences = { 
+            "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+            "extensions.pocket.enabled" = lock-false;
+            "extensions.screenshots.disabled" = lock-true;
+            # add global preferences here...
+          };
+        };
+      };
+
+      /* ---- PROFILES ---- */
+      # Switch profiles via about:profiles page.
+      # For options that are available in Home-Manager see
+      # https://nix-community.github.io/home-manager/options.html#opt-programs.firefox.profiles
+      profiles ={
+        arteii = {           # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
+          id = 0;               # 0 is the default profile; see also option "isDefault"
+          name = "arteii";   # name as listed in about:profiles
+          isDefault = true;     # can be omitted; true if profile ID is 0
+          settings = {          # specify profile-specific preferences here; check about:config for options
+            "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+            "browser.startup.homepage" = "https://github.com";
+            "browser.newtabpage.pinned" = [
+	      {
+                title = "NixOS";
+                url = "https://nixos.org";
+              }
+	      {
+		title = "NixOS Search";
+		url = "https://search.nixos.org/packages";
+	      }
+	      {
+	        title = "GitHub";
+		url = "https://github.com";
+	      }
+	    ];
+            # add preferences for profile_0 here...
+          };
+        };
+      # add profiles here...
+      };
+    };
+  };
+}
