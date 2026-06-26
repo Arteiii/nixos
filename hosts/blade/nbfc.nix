72c99410f96c2665198536660abca0fb05be43f8
Ben Pilger <bpilger@sparx.foundation>
2026-06-26 16:47:59 +0200

    nixos-switch: auto-commit 2026-06-26 16:47:59

--- hosts/blade/nbfc.nix
+++ hosts/blade/nbfc.nix
@@ -3,51 +3,24 @@
   pkgs,
   ...
 }:
-let
-  nbfcConfig = builtins.toJSON {
-    notebook_model = "Razer Blade";
-    fan_configurations = [
-      {
-        fan_speed_write_cycles = 100;
-        read_write_cycles = 100;
-        temperature_thresholds = [
-          {
-            temperature = 35;
-            fan_speed = 40;
-          }
-          {
-            temperature = 45;
-            fan_speed = 60;
-          }
-          {
-            temperature = 55;
-            fan_speed = 85;
-          }
-          {
-            temperature = 65;
-            fan_speed = 100;
-          }
-        ];
-      }
-    ];
-  };
-in
 {
-
-  environment.etc."nbfc.json".text = nbfcConfig;
-
   environment.systemPackages = [
     inputs.nbfc-linux.packages.x86_64-linux.default
   ];
+
   systemd.services.nbfc_service = {
     enable = true;
     description = "NoteBook FanControl service (JSON-Mode)";
+
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${inputs.nbfc-linux.packages.x86_64-linux.default}/bin/nbfc_service --config-file /etc/nbfc.json";
+      ExecStart = "${inputs.nbfc-linux.packages.x86_64-linux.default}/bin/nbfc_service --auto-fan-control";
       Restart = "always";
+      RestartSec = "5";
     };
+
     path = [ pkgs.kmod ];
+
     wantedBy = [ "multi-user.target" ];
   };
 }
