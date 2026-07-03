3f462e0080f4248646261315ee654753e58fc713
Ben Pilger <bpilger@sparx.foundation>
2026-07-03 16:11:03 +0200

    nixos-switch: auto-commit 2026-07-03 16:11:03

--- users/arteii/ssh.nix
+++ users/arteii/ssh.nix
@@ -5,18 +5,18 @@
     enable = true;
     enableDefaultConfig = false;
 
-    settings = {
-      ControlMaster = "auto";
-      ControlPath = "~/.ssh/sockets/%r@%h-%p";
-      ControlPersist = "10m";
-    };
-
     matchBlocks = {
       "*" = {
         identityFile = [
           "~/.ssh/id_ed25519"
         ];
         forwardAgent = false;
+
+        extraOptions = {
+          ControlMaster = "auto";
+          ControlPath = "~/.ssh/sockets/%r@%h-%p";
+          ControlPersist = "10m";
+        };
       };
     };
   };
